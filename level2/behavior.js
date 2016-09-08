38acc62c0d8d2f6b3e9e3036c036f10af4fda916
Yoshiyasu5046 <yoshi.kitahara.niapat@gmail.com>
2016-09-08 00:04:08 -0700

    no message

---
+++ level2/behavior.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+	var thumbnailElement = document.getElementById("smart_thumbnail");
+	thumbnailElement.className = "";
+	// クリックされたものの色を変える
+});
