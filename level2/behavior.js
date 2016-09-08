c92de6bed48610bcf23ad60d21c0b0cc58eea995
Yoshiyasu5046 <yoshi.kitahara.niapat@gmail.com>
2016-09-08 01:10:59 -0700

    no message

--- level2/behavior.js
+++ level2/behavior.js
@@ -1,5 +1,12 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 	var thumbnailElement = document.getElementById("smart_thumbnail");
-	thumbnailElement.className = "";
-	// クリックされたものの色を変える
+	thumbnailElement.addEventListener("click", function() {
+		// if clicked, then make the img bigger
+		if (thumbnailElement.className == "") {
+			thumbnailElement.className = "small";
+		} else if (thumbnailElement.className == "small") {
+			thumbnailElement.className = "";
+		}
+		// クリックされたものの色を変える
+	});
 });
